47e7115b4e9f4ada0346c6a3a6aa46c96f3e271c
Osama Hamad <osamahama2005@gmail.com>
2021-08-06 14:09:08 -0400

    add settings csv file feature

--- convert.py
+++ convert.py
@@ -1,14 +1,15 @@
 import cv2 as cv
 import os
+from csv import reader
 
 
-def to_video(image_sequence):
+def to_video(image_sequence, output, filetype, codec, fps):
     for num in image_sequence:
 
         height, width, layers = num.shape
         size = (width, height)
 
-    out = cv.VideoWriter('output.mp4', cv.VideoWriter_fourcc(*'mp4v'), 15, size)
+    out = cv.VideoWriter(f'{output}.{filetype}', cv.VideoWriter_fourcc(*codec), fps, size)
 
     for i in image_sequence:
         out.write(i)
@@ -16,9 +17,16 @@ def to_video(image_sequence):
 
 
 def main():
-    # name of the image sequence dir
-    dir_name = 'heart_pound'
     image_sequence = []
+
+    with open('settings.csv') as settings:
+        settings = list(reader(settings))
+
+    dir_name = settings[0][1]
+    output = settings[1][1]
+    fps = int(settings[2][1])
+    file_type = settings[3][1]
+    codec = settings[4][1]
 
     # check if the dir with the image sequence exists
     if os.path.isdir(dir_name):
@@ -28,7 +36,7 @@ def main():
 
             image_sequence.append(cv.imread(f'{dir_name}/{image}'))
 
-        to_video(image_sequence)
+        to_video(image_sequence, output, file_type, codec, fps)
 
     else:
         print('Error: Image directory not found. Ensure that directory is in path.')
